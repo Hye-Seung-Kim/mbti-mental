c5c6445e3a65427dcebff86c5e9022a63843ec87
hyeseung <hyeseung8526@gmail.com>
2025-06-21 21:59:30 +0900

    Remove broken submodule and re-add wellness-app folder

---
+++ wellness-app/src/App.js
@@ -0,0 +1,12 @@
+import WellnessApp from './WellnessApp';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <WellnessApp />
+    </div>
+  );
+}
+
+export default App;
